94a59a3766916f81bcc856769035735898738dc7
tiltedlistener <tiltedlistener@gmail.com>
2014-07-16 00:46:11 -0700

    Filling out the further functions of Terrain, Adding JFrame to TerrainGeneration, Adding Point Class

--- src/main/Terrain.java
+++ src/main/Terrain.java
@@ -1,37 +1,41 @@
 package main;
 
 import java.util.*;
+import java.awt.Graphics2D;
+import java.awt.Color;
 
 public class Terrain {
 
-	public double[] map;
+	public int[] map;
 	public int size;
 	public int max;
+	public Graphics2D g2d;
 	
 	public Random rand;
 	
-	public Terrain(double detail) {
+	public Terrain(double detail, Graphics2D _g2d) {
 		rand = new Random();
 		
+		this.g2d = _g2d;
 		this.size = (int)Math.pow(2, detail) + 1; 
 		this.max = this.size - 1;
-		this.map = new double[(int)(this.size * this.size)];
+		this.map = new int[(this.size * this.size)];
 	}
 	
-	public double get(int x, int y) {
+	public int get(int x, int y) {
 		if (x<0 || x > this.max || y<0 || y > this.max) return -1;
 		return this.map[(int)(x + this.size*y)];
 	}
 	
-	public void set(int x, int y, double val) {
+	public void set(int x, int y, int val) {
 		this.map[(int)(x + this.size*y)] = val;
 	}
 	
 	public void generate(int roughness) {
 		this.set(0, 0, this.max);
-		this.set(this.max, 0, (double)(this.max / 2));
+		this.set(this.max, 0, (this.max / 2));
 		this.set(this.max, this.max, 0);
-		this.set(0, this.max, (double)(this.max / 2));
+		this.set(0, this.max, (this.max / 2));
 		
 		this.divide(this.max, roughness);
 	}
@@ -57,11 +61,78 @@ public class Terrain {
 		this.divide(size / 2, roughness);
 	}
 	
-	public void square(int x, int y, int half, int scale) {
+	public int average(int[] values) {
+		int length = values.length;
+		
+		List<Integer> valid = new ArrayList<Integer>();
+		for(int i=0;i<length;i++) {
+			if (values[i] != -1) {
+				valid.add(values[i]);
+			}
+		}
+		
+		int sum = 0;
+		for(int j=0;j<valid.size();j++) {
+			sum+= valid.get(j);
+		}
+		
+		return sum/valid.size();
+	}
+	
+	public void square(int x, int y, int sze, int offset) {		
+		int[] toAvg = {
+		           this.get(x - sze, y - sze),   // upper left
+		           this.get(x + sze, y - sze),   // upper right
+		           this.get(x + sze, y + sze),   // lower right
+		           this.get(x - sze, y + sze)    // lower left
+		        };
+		
+		int ave = this.average(toAvg);
+		this.set(x, y, ave + offset);
+	}
+	
+	public void diamond(int x, int y, int sze, int offset) {
+		int[] toAvg = {
+		           this.get(x, y - sze),   // upper left
+		           this.get(x + sze, y),   // upper right
+		           this.get(x, y + sze),   // lower right
+		           this.get(x - sze, y)    // lower left
+		        };
+		
+		int ave = this.average(toAvg);
+		this.set(x, y, ave + offset);
+	}
+	
+	public void draw() {
+		double waterVal = this.size * 0.3;
+		
+		 for (int y = 0; y < this.size; y++) {
+		      for (int x = 0; x < this.size; x++) {
+		        int val = this.get(x, y);
+		        Point top = this.project(x, y, val);
+		        Point bottom = this.project(x + 1, y, 0);
+		        Point water = this.project(x, y, waterVal);
+		        String style = this.brightness(x, y, this.get(x + 1, y) - val);
+		
+		        this.rect(top, bottom, style);
+		        this.rect(water, bottom, "rgba(50, 150, 200, 0.15)");
+		      }
+		 }
+	}
+	
+	public Point project(int flatX, int flatY, double flatZ) {
+		
+		
+		return new Point();
+	}
+	
+	public String brightness(int x, int y, int slope) {
+		
+		return " ";
+	}
+	
+	public void rect(Point a, Point b, String style) {
 		
 	}
 	
-	public void diamond(int x, int y, int half, int scale) {
-		
-	}
 }
--- src/main/TerrainGeneration.java
+++ src/main/TerrainGeneration.java
@@ -1,9 +1,20 @@
 package main;
 
-public class TerrainGeneration {
+import javax.swing.*;
+
+public class TerrainGeneration extends JFrame {
+	
+	static final long serialVersionUID = 0;
 
 	public static void main(String[] args) {
+		new TerrainGeneration();
+	}
 	
+	public TerrainGeneration() {
+		super("Terrain!");
+		setSize(640,480);
+		setVisible(true);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
 
 }
